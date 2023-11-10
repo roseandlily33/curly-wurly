1dc9e30f6949c129dc1154e74caa94720eb52c67
Victoria Benoit <vrose834@gmail.com>
2023-11-10 15:35:18 -0400

    Can add a todo

--- main/src/Main.js
+++ main/src/Main.js
@@ -1,12 +1,19 @@
 import {useState} from 'react';
-import AddTodo from './Components/AddTodo';
 const Main = () => {
-    const [todos, setTodos] = useState(['one', 'two']);
+    const[todos, setTodos] = useState(['one', 'two']);
+    const[newTodo, setNewTodo] = useState('');
+    const handleSubmit = () => {
+        setTodos([...todos, newTodo]);
+        console.log('mytodos', todos);
+    };
 
     console.log(todos)
     return ( 
         <main>
-            <AddTodo todos={todos}/>
+        <div>
+            <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)}></input>
+            <button type="submit" onClick={handleSubmit}>Add Todo</button>
+        </div>
         </main>
 
      );
